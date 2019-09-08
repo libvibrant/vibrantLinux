a712784a0ba16e77360dc4f95eb9713daf44e1f0
zee220 <mustafazaid2172000@gmail.com>
2019-09-08 13:21:09 -0600

    update now removes all assets as they are no longer needed

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@ git reset --hard
 git pull
 qmake
 make
-rm *.o *.h *.cpp *.ui *.pro* Makefile LICENSE README.md assets/program.png assets/entryeditor.png
+rm *.o *.h *.cpp *.ui *.pro* Makefile LICENSE README.md assets/ -r
