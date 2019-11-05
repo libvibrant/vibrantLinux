61ac3c5bd339055de2320e3f772cd17941b21afd
zee <mustafa@zee.technology>
2019-11-04 19:26:39 -0700

    don't run program if it's already running. Useful for users who auto start Vibrant Linux but who's DE restores their previous session

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
 #include "mainwindow.h"
+
 #include <QApplication>
 #include <QCommandLineParser>
+#include <QDir>
+#include <QFileInfo>
+
+#include <unistd.h>
+
+#include <iostream>
 
 int main(int argc, char *argv[]){
+	//don't run if the program is already running
+	QString vlPath = QFileInfo("/proc/"+QString::number(getpid())+"/exe").canonicalPath();
+	QStringList procs = QDir("/proc").entryList(QDir::Filter::Dirs);
+	procs = procs.filter("^\\d+$");
+	for(auto &proc: procs){
+		if(vlPath == QFileInfo("/proc/"+proc+"/exe").canonicalPath()){
+			std::cout << "Vibrant Linux is already running" << std::endl;
+			return EXIT_SUCCESS;
+		}
+	}
+
 	QApplication a(argc, argv);
 	QCommandLineParser parser;
 	parser.addOption(QCommandLineOption("hidden"));
