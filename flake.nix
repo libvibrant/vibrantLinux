21ed30dee2f762e1f3b0cc0f24c2a454d59d546c
Sefa Eyeoglu <contact@scrumplex.net>
2024-04-14 13:46:23 +0200

    feat(nix): add git-hooks
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- flake.nix
+++ flake.nix
@@ -1,22 +1,58 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    git-hooks.url = "github:cachix/git-hooks.nix";
+    git-hooks.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
   outputs =
-    { nixpkgs, ... }:
+    {
+      git-hooks,
+      nixpkgs,
+      self,
+      ...
+    }:
     let
       forAllSystems =
         function:
         nixpkgs.lib.genAttrs [
           "x86_64-linux"
           "aarch64-linux"
-        ] (system: function nixpkgs.legacyPackages.${system});
+        ] (system: function nixpkgs.legacyPackages.${system} system);
     in
     {
-      packages = forAllSystems (pkgs: rec {
-        default = vibrantlinux;
-        vibrantlinux = pkgs.qt6Packages.callPackage ./nix/package.nix { };
-      });
+      packages = forAllSystems (
+        pkgs: _: rec {
+          default = vibrantlinux;
+          vibrantlinux = pkgs.qt6Packages.callPackage ./nix/package.nix { };
+        }
+      );
 
-      formatter = forAllSystems (pkgs: pkgs.nixfmt-rfc-style);
+      checks = forAllSystems (
+        pkgs: system: {
+          pre-commit-check = git-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              nixfmt = {
+                enable = true;
+                package = self.formatter.${system};
+              };
+            };
+          };
+        }
+      );
+
+      devShells = forAllSystems (
+        pkgs: system: {
+          default = pkgs.mkShell {
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+            buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+
+            inputsFrom = [ self.packages.${system}.vibrantlinux ];
+          };
+        }
+      );
+
+      formatter = forAllSystems (pkgs: _: pkgs.nixfmt-rfc-style);
     };
 }
